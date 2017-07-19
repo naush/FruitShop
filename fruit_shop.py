635dac7fe497a0646261611eb1cfd5e0fa39a32e
Li-Hsuan Lung <lihsuan@8thlight.com>
2017-07-19 12:59:08 -0400

    Iteration 0

--- fruit_shop.py
+++ fruit_shop.py
@@ -1,9 +1,24 @@
+
+
 if __name__ == "__main__":
   fruitPrice = {"Apple": 100, "Cherry": 75, "Banana": 150}
+  # fruitDiscounts = {"Cherry": [2, 20, 0]}
+  cherries = []
 
   price = 0
   while True:
     res = raw_input("")
+
+    if res == "Cherry":
+        cherries.append(res)
+
     price += fruitPrice.get(res, 0)
+
+    # fruitDiscount = fruitDiscounts.get(res, [])
+
+    if len(cherries) == 2:
+        price = price - 20
+        cherries = []
+
     print price
 
